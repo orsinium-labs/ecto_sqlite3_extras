0161c9e96d8eb6397fb59f5c5b63c26df9ab07ff
gram <git@orsinium.dev>
2023-01-30 16:18:40 +0100

    improve metadata

--- mix.exs
+++ mix.exs
@@ -1,31 +1,49 @@
 defmodule EctoSQLite3Extras.MixProject do
   use Mix.Project
+  @github_url "https://github.com/orsinium-labs/ecto_sqlite3_extras"
+  @version "1.1.5"
 
   def project do
     [
       app: :ecto_sqlite3_extras,
-      version: "1.0.0",
+      version: @version,
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      deps: deps(),
+      package: package(),
+      docs: docs()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
-  def application do
-    [
-      extra_applications: [:logger]
-    ]
-  end
-
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:exqlite, "~> 0.13.2"},
       {:table_rex, "~> 3.1.1"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:ecto_sqlite3, "~> 0.9.1", only: [:test]}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp description do
+    """
+    Helpful queries and Phoenix Live Dashboard integration for SQLite.
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["Gram"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @github_url}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      source_url: @github_url,
+      extras: ["README.md"]
     ]
   end
 end
